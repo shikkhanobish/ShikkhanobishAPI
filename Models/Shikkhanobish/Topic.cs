510c54d49cfada757e4607384ee3c84dde0bf70a
Abid Hassan <abidhassandiba@gmail.com>
2022-01-10 17:45:09 +0600

    Topic

---
+++ Models/Shikkhanobish/Topic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SHikkhanobishAPI.Models.Shikkhanobish
+{
+    public class Topic
+    {
+        public int topicID { get; set; }
+        public int chapterID { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string Response { get; set; }
+
+    }
+}
